ee9edbd81d01b96d2fefd956f4eeb6269ea52c4e
Alal Uddin <alalu75666@gmail.com>
2023-04-02 09:17:52 +0530

    order_modal_created

---
+++ server/modals/order.modal.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "product",
+        },
+        quantity: Number,
+      },
+    ],
+    totalAmount: { 
+        type: Number
+     },
+    payment: {
+      type: String,
+      enum: ["cashOn", "UPI", "netBanking"],
+      default: "cashOn",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "packed", "dispatch", "cancelled", "deliverd"],
+      default: "pending",
+    },
+    created: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+// order model
+
+const orderModel = mongoose.model("order", orderSchema);
+
+module.export=orderModel
