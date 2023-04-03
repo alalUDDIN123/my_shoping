2b8e0369095ddd838df7da316e18845bf8d27196
Alal Uddin <alalu75666@gmail.com>
2023-04-03 11:40:47 +0530

    remove/updateStatus/allorders/totalDelivered_in_order_done

--- server/controller/orders.controller.js
+++ server/controller/orders.controller.js
@@ -22,7 +22,7 @@ const PostOrder = async (req, res) => {
 
     res.status(201).send({ message: "Order placed successfully" });
   } catch (error) {
-    console.error(error);
+
     res.status(500).send({ message: "Internal server error" });
   }
 };
@@ -56,10 +56,10 @@ const GetOrders = async (req, res) => {
       }, 0);
       return total + orderTotal;
     }, 0);
-   
+
     res.status(200).send({ orders, totalPrice });
   } catch (error) {
-    console.error(error);
+
     res.status(500).send({ message: "Internal server error" });
   }
 };
@@ -67,11 +67,26 @@ const GetOrders = async (req, res) => {
 
 // get single order  👍👍👍
 
-const getSingleOrder= async(req,res)=>{
-  const { orderId } = req.body;
-  
+const getSingleOrder = async (req, res) => {
+  const { orderId, productId, userId } = req.body;
+
+  if (!orderId || !productId) {
+    return res.status(400).json({ message: 'orderId and productId is required' });
+  }
+
   try {
-    const order = await orderModel.findById(orderId)
+
+    const order = await orderModel.findById(orderId);
+
+    if (!order) {
+      return res.status(404).json({ message: 'Order not found' });
+    }
+    // console.log("orders::-", order)
+
+    if (order.user.toString() !== userId) {
+      return res.status(401).json({ message: 'You are not authorized to get this order' });
+    }
+    const singleOrder = await orderModel.findOne({ "products.productId": productId })
       .populate({
         path: "products.productId",
         model: "product",
@@ -81,23 +96,137 @@ const getSingleOrder= async(req,res)=>{
         model: "deliveryAddres",
       })
       .lean();
-    if (!order) {
+
+    if (!singleOrder) {
       return res.status(404).json({ message: 'Order not found' });
     }
-    const totalPrice = order.products.reduce((acc, cur) => {
-      return acc + cur.productId.discountPrice;
-    }, 0);
-    res.status(200).json({ order, totalPrice });
+    // console.log("single order::-", singleOrder)
+
+    res.status(200).json({ singleOrder });
   } catch (err) {
-    console.error(err);
+
     res.status(500).json({ message: 'Internal server error' });
   }
 }
 
+const getallOrders = async (req, res) => {
+  const orders = await orderModel.find();
+  try {
+    res.status(200).send({ orders });
+  } catch (err) {
+    res.status(500).send({ msg: "Something went wrong", error: err.message });
+  }
+};
+
+
+// remove order  👍👍👍
+
+const removeOrder = async (req, res) => {
+  const { orderId, productId, userId } = req.body;
+
+  if (!orderId || !productId) {
+    return res.status(400).json({ message: 'orderId and productId is required' });
+  }
+
+  try {
+
+
+
+    const order = await orderModel.findById(orderId);
+
+    if (!order) {
+      return res.status(404).json({ message: 'Order not found' });
+    }
+    // console.log("orders::-", order)
+
+    if (order.user.toString() !== userId) {
+      return res.status(401).json({ message: 'You are not authorized to remove this order' });
+    }
+
+    if (order.orderStatus === "delivered") {
+      return res.status(403).json({ message: "Cannot remove a delivered order" });
+    }
+
+    await orderModel.findOneAndRemove({ "products.productId": productId });
+    res.status(200).json({ message: "Order removed successfully" });
+  } catch (err) {
+
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+// update order status 👍👍👍
+
+const updateOrderStatus = async (req, res) => {
+  const { orderId, newOrderStatus } = req.body;
+
+
+  if (!orderId || !newOrderStatus) {
+    return res.status(400).json({ message: 'orderId and newOrderStatus is required' });
+  }
+
+  try {
+    const existingOrder = await orderModel.findById(orderId);
+
+    if (!existingOrder) {
+      return res.status(404).json({ message: 'Order not found' });
+    }
+
+    if (existingOrder.orderStatus === "delivered") {
+      return res.status(403).json({ message: "Cannot update a delivered order" });
+    }
+
+    if (existingOrder.orderStatus === newOrderStatus) {
+      return res.status(400).json({ message: 'Order status already set to the new status' });
+    }
+
+    if (!["pending", "packed", "dispatch", "cancelled", "delivered"].includes(newOrderStatus)) {
+      return res.status(400).send({ message: "Valid orderStatus required" });
+    }
+
+    existingOrder.orderStatus = newOrderStatus;
+    await existingOrder.save();
+    res.status(200).json({ message: 'Order status updated successfully' });
+  } catch (err) {
+
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const countDeliveredOrders = async (req, res) => {
+  try {
+
+    const deliveredOrders = await orderModel
+      .find({ orderStatus: "delivered" })
+      .populate({
+        path: "products.productId",
+        model: "product",
+      })
+      .lean();
+
+    const totalAmount = deliveredOrders.reduce((acc, cur) => {
+      return (
+        acc +
+        cur.products.reduce((acc2, cur2) => {
+          return acc2 + cur2.productId.discountPrice;
+        }, 0)
+      );
+    }, 0);
+    // console.log(deliveredOrders)
+    res.status(200).json({ totalOrdersDelivered: deliveredOrders.length, totalAmount });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
 
 
 module.exports = {
   PostOrder,
   GetOrders,
-  getSingleOrder
+  getSingleOrder,
+  getallOrders,
+  removeOrder,
+  updateOrderStatus,
+  countDeliveredOrders
 }
