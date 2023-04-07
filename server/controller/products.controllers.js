8f0df4c11dc1271f68d65659dbdaa573afa470f9
Alal Uddin <alalu75666@gmail.com>
2023-04-07 11:52:57 +0530

    review_feature_done

--- server/controller/products.controllers.js
+++ server/controller/products.controllers.js
@@ -1,5 +1,6 @@
 const { default: mongoose } = require("mongoose");
-const productModal = require("../modals/product.modal")
+const productModal = require("../modals/product.modal");
+const userModal = require("../modals/user.modal");
 
 // creating or adding product (admin) 👍👍👍👍
 
@@ -158,8 +159,8 @@ const updateProduct = async (req, res) => {
 const deleteProduct = async (req, res) => {
   let product_Id = req.body.product_id;
 
-  if(!product_Id){
-      return res.status(400).send({ message: "Required product_id " });  
+  if (!product_Id) {
+    return res.status(400).send({ message: "Required product_id " });
   }
   try {
     const isObjectIdValid = mongoose.Types.ObjectId.isValid(product_Id)
@@ -187,11 +188,199 @@ const deleteProduct = async (req, res) => {
   }
 }
 
+
+
+// create new review or update 👍👍👍👍
+
+const createReview = async (req, res, next) => {
+  try {
+    const { rating, comment, productId, userId } = req.body;
+
+    const findUser = await userModal.findById({ _id: userId })
+
+    if (!findUser) {
+      return res.status(404).send({ message: "User not found" })
+    }
+
+    const review = {
+      user: userId,
+      name: findUser.name,
+      rating: Number(rating),
+      comment,
+    };
+
+    const product = await productModal.findById(productId);
+    if (!product) {
+      return res.status(404).send({ message: "product does not exits" })
+    }
+
+    const isUserAlreadyReviewd = product.reviews.find(
+      (rev) => rev.user.toString() === userId.toString()
+    );
+
+
+    if (isUserAlreadyReviewd) {
+      product.reviews.forEach((rev) => {
+        if (rev.user.toString() === userId.toString()) {
+          rev.rating = rating;
+          rev.comment = comment;
+        }
+      });
+
+
+
+      let avg = 0;
+      product.reviews.forEach((rev) => {
+        avg += rev.rating;
+      });
+
+      // console.log("total reviews", product.reviews.length);
+      // console.log("total rating", avg);
+      // console.log("average rating", product.ratings);
+      // console.log("product ratings", Math.ceil(avg / product.reviews.length));
+
+      product.ratings = Math.ceil(avg / product.reviews.length);
+
+      await product.save();
+      res.status(200).send({
+        message: "Review updated"
+      });
+    } else {
+      product.reviews.push(review);
+      product.numOfReviews = product.reviews.length;
+
+      let avg = 0;
+
+      product.reviews.forEach((rev) => {
+        avg += rev.rating;
+      });
+      // console.log("total reviews avearge",avg);
+
+      product.ratings = Math.ceil(avg / product.reviews.length);
+
+      await product.save();
+      res.status(200).send({
+        message: "Review Added"
+      });
+    }
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+// Get All Reviews of a product 👍👍👍👍
+
+const getAllReviewOfAproduct = async (req, res) => {
+  const { productId } = req.body;
+
+  try {
+    if (!productId) {
+      return res.status(400).send({ message: "Required productId " });
+    }
+    const product = await productModal.findById(productId);
+    if (!product) {
+      return res.status(404).send({ message: "product does not exits" })
+    }
+
+    res.status(200).send({
+      reviews: product.reviews,
+    });
+  } catch (error) {
+    res.status(500).send({ message: "Server error", err: error.message })
+  }
+}
+
+// get Single review of  a product 👍👍👍👍
+
+const getSingleReview = async (req, res) => {
+  const { productId} = req.body;
+  const reviewId =req.params.reviewId 
+
+
+  try {
+    if (!productId) {
+      return res.status(400).send({ message: "Required productId" });
+    }
+
+    if (!reviewId) {
+      return res.status(400).send({ message: "Required reviewId" });
+    }
+
+    const product = await productModal.findById(productId);
+    if (!product) {
+      return res.status(404).send({ message: "Product does not exist" });
+    }
+  
+
+    const review = product.reviews.find(review => review._id.toString() === reviewId);
+    if (!review) {
+      return res.status(404).send({ message: "Review does not exist" });
+    }
+
+    res.status(200).send({
+      review,
+    });
+  } catch (error) {
+    res.status(500).send({ message: "Server error", err: error.message })
+  }
+}
+
+// delete a review (SuperAdmin) 👍👍👍👍
+const deleteReview = async (req, res) => {
+  const { productId, reviewId } = req.body;
+
+  try {
+    if (!productId) {
+      return res.status(400).send({ message: "Required productId" });
+    }
+
+    if (!reviewId) {
+      return res.status(400).send({ message: "Required reviewId" });
+    }
+
+    const product = await productModal.findById(productId);
+    if (!product) {
+      return res.status(404).send({ message: "Product does not exist" });
+    }
+
+    const review = product.reviews.find(rev => rev._id.toString() === reviewId);
+    if (!review) {
+      return res.status(404).send({ message: "Review does not exist" });
+    }
+
+    // remove the review from the reviews array
+    const filteredReviews = product.reviews.filter(rev => rev._id.toString() !== reviewId);
+    product.reviews = filteredReviews;
+
+    // update the product's ratings and numOfReviews fields after removing review
+    const totalRating = product.reviews.reduce((sum, review) => sum + review.rating, 0);
+    const numOfReviews = product.reviews.length;
+    const averageRating = totalRating / numOfReviews;
+    product.ratings = parseFloat(averageRating.toFixed(1));
+    product.numOfReviews = numOfReviews;
+
+    await product.save();
+    res.status(200).send({
+      message: "Review deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).send({ message: "Server error", err: error.message })
+  }
+}
+
+
 module.exports = {
   createProduct,
   getProducts,
   getSingleProduct,
   updateProduct,
-  deleteProduct
+  deleteProduct,
+
+  createReview,
+  getAllReviewOfAproduct,
+  getSingleReview,
+  deleteReview 
 
 }
