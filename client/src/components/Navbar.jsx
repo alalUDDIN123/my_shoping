186a8b291626da1c4c6d765724abd8d3d19672e9
Ravi-047 <ravi047raj@gmail.com>
2023-04-10 18:22:30 +0530

    search-fun

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,22 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { AiOutlineShoppingCart, AiOutlineMenu,AiOutlineClose } from 'react-icons/ai';
-import { ImProfile } from 'react-icons/im';
-import { BsBagCheck } from 'react-icons/bs';
-import { GrList } from 'react-icons/gr';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  AiOutlineShoppingCart,
+  AiOutlineMenu,
+  AiOutlineClose,
+} from "react-icons/ai";
+import { ImProfile } from "react-icons/im";
+import { BsBagCheck } from "react-icons/bs";
+import { GrList } from "react-icons/gr";
 
-import styles from '../styles/navbar.module.css';
-import { useMediaQuery } from 'react-responsive';
+import styles from "../styles/navbar.module.css";
+import { useMediaQuery } from "react-responsive";
+import SearchInput from "./SearchInput";
 
 function Navbar({ isLoggedIn = true, cartItemsCount = 2 }) {
   // const [showMenu, setShowMenu] = useState(false);
   const [showRightSide, setShowRightSide] = useState(false);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const Desktop = ({ children }) => {
     const isDesktop = useMediaQuery({ minWidth: 992 });
@@ -26,26 +31,18 @@ function Navbar({ isLoggedIn = true, cartItemsCount = 2 }) {
     return isMobile ? children : null;
   };
 
-
-
-
-
-
-  const onLogout = () => { };
+  const onLogout = () => {};
 
   return (
     <>
       <Desktop>
-
         {/* for default */}
         <nav className={styles.navbar}>
           <div className={styles.logo}>
-            <h1 onClick={() => navigate("/")} >
-              myShopping
-            </h1>
+            <h1 onClick={() => navigate("/")}>myShopping</h1>
           </div>
           <div className={styles.search}>
-            <input type="text" placeholder="Search products..." />
+            <SearchInput />
           </div>
           <div className={styles._rightSide}>
             {isLoggedIn ? (
@@ -53,13 +50,12 @@ function Navbar({ isLoggedIn = true, cartItemsCount = 2 }) {
                 <button onClick={onLogout}>Admin</button>
                 <button onClick={onLogout}>Logout</button>
                 <div className={styles.more}>
-                  <button >
-                    More...
-
-                  </button>
+                  <button>More...</button>
                   <div className={styles.dropdown}>
                     <Link to="/profile">
-                      <ImProfile style={{ marginRight: "10px", color: "white" }} />
+                      <ImProfile
+                        style={{ marginRight: "10px", color: "white" }}
+                      />
                       Profile
                     </Link>
                     <Link to="/orders">
@@ -67,10 +63,11 @@ function Navbar({ isLoggedIn = true, cartItemsCount = 2 }) {
                       Wishlist
                     </Link>
                     <Link to="/orders">
-                      <BsBagCheck style={{ marginRight: "10px", color: "white" }} />
+                      <BsBagCheck
+                        style={{ marginRight: "10px", color: "white" }}
+                      />
                       Orders
                     </Link>
-
                   </div>
                 </div>
                 <div className={styles.cartIcon}>
@@ -95,71 +92,73 @@ function Navbar({ isLoggedIn = true, cartItemsCount = 2 }) {
       <Tablet>
         <nav className={styles._tablet_navbar}>
           <div className={styles._tablet_logo}>
-            <h1 onClick={() => navigate("/")}>
-              myShopping
-            </h1>
+            <h1 onClick={() => navigate("/")}>myShopping</h1>
           </div>
           <div className={styles._tablet_search}>
             <input type="text" placeholder="Search products..." />
           </div>
 
-          <div className={`${styles._tablet_rightSide} ${showRightSide ? "showRightSide" : ""}`}>
-            <div className={styles.hamburgerIcon} onClick={() => setShowRightSide(!showRightSide)}>
-              {showRightSide ? <AiOutlineClose /> :<AiOutlineMenu /> }
-              
+          <div
+            className={`${styles._tablet_rightSide} ${
+              showRightSide ? "showRightSide" : ""
+            }`}
+          >
+            <div
+              className={styles.hamburgerIcon}
+              onClick={() => setShowRightSide(!showRightSide)}
+            >
+              {showRightSide ? <AiOutlineClose /> : <AiOutlineMenu />}
             </div>
-            {showRightSide &&
-              (
-                <div className={styles._tablet_rightSidebar}>
-                  {isLoggedIn ? (
-                    <>
-                      <ul>
-                        <li>
-                          <a href="/profile">
-                            <ImProfile style={{ marginRight: "10px" }} />
-                            Profile
-                          </a>
-                        </li>
-                        <li>
-                          <a href="/profile">
-                            <GrList style={{ marginRight: "10px" }} />
-                            Wishlist
-                          </a>
-                        </li>
-                        <li>
-                          <a href="/profile">
-                            <AiOutlineShoppingCart style={{ marginRight: "10px", fontSize: "25px" }} />
-                            {cartItemsCount > 0 && <span>{cartItemsCount}</span>}
-                          </a>
-                        </li>
-                        <li>
-                          <a href="/profile">
-                            <BsBagCheck style={{ marginRight: "10px" }} />
-                            Orders
-                          </a>
-                        </li>
-                      </ul>
-                    </>
-                  ) : (
-                    <>
-                      <ul>
-                        <li><a href="/signup">Signup</a></li>
-                        <li><a href="/login">Login</a></li>
-
-                      </ul>
-                    </>
-                  )}
-                </div>
-              )
-
-
-            }
+            {showRightSide && (
+              <div className={styles._tablet_rightSidebar}>
+                {isLoggedIn ? (
+                  <>
+                    <ul>
+                      <li>
+                        <a href="/profile">
+                          <ImProfile style={{ marginRight: "10px" }} />
+                          Profile
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/profile">
+                          <GrList style={{ marginRight: "10px" }} />
+                          Wishlist
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/profile">
+                          <AiOutlineShoppingCart
+                            style={{ marginRight: "10px", fontSize: "25px" }}
+                          />
+                          {cartItemsCount > 0 && <span>{cartItemsCount}</span>}
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/profile">
+                          <BsBagCheck style={{ marginRight: "10px" }} />
+                          Orders
+                        </a>
+                      </li>
+                    </ul>
+                  </>
+                ) : (
+                  <>
+                    <ul>
+                      <li>
+                        <a href="/signup">Signup</a>
+                      </li>
+                      <li>
+                        <a href="/login">Login</a>
+                      </li>
+                    </ul>
+                  </>
+                )}
+              </div>
+            )}
           </div>
         </nav>
       </Tablet>
-
-
-
 
       {/* for mobile */}
 
@@ -171,65 +170,68 @@ function Navbar({ isLoggedIn = true, cartItemsCount = 2 }) {
             </h1>
           </div>
 
-
-
-
-          <div className={`${styles._tablet_rightSide} ${showRightSide ? "showRightSide" : ""}`}>
-            <div className={styles.hamburgerIcon} onClick={() => setShowRightSide(!showRightSide)}>
-            {showRightSide ? <AiOutlineClose /> :<AiOutlineMenu /> }
+          <div
+            className={`${styles._tablet_rightSide} ${
+              showRightSide ? "showRightSide" : ""
+            }`}
+          >
+            <div
+              className={styles.hamburgerIcon}
+              onClick={() => setShowRightSide(!showRightSide)}
+            >
+              {showRightSide ? <AiOutlineClose /> : <AiOutlineMenu />}
             </div>
-            {showRightSide &&
-              (
-                <div className={styles._tablet_rightSidebar}>
-                  {isLoggedIn ? (
-                    <>
-                      <ul>
-                        <li>
-                          <a href="/profile">
-                            <ImProfile style={{ marginRight: "10px" }} />
-                            Profile
-                          </a>
-                        </li>
-                        <li>
-                          <a href="/profile">
-                            <GrList style={{ marginRight: "10px" }} />
-                            Wishlist
-                          </a>
-                        </li>
-                        <li>
-                          <a href="/profile">
-                            <AiOutlineShoppingCart style={{ marginRight: "10px", fontSize: "25px" }} />
-                            {cartItemsCount > 0 && <span>{cartItemsCount}</span>}
-                          </a>
-                        </li>
-                        <li>
-                          <a href="/profile">
-                            <BsBagCheck style={{ marginRight: "10px" }} />
-                            Orders
-                          </a>
-                        </li>
-                      </ul>
-                    </>
-                  ) : (
-                    <>
-                      <ul>
-                        <li><a href="/signup">Signup</a></li>
-                        <li><a href="/login">Login</a></li>
-
-                      </ul>
-                    </>
-                  )}
-                </div>
-              )
-
-
-            }
+            {showRightSide && (
+              <div className={styles._tablet_rightSidebar}>
+                {isLoggedIn ? (
+                  <>
+                    <ul>
+                      <li>
+                        <a href="/profile">
+                          <ImProfile style={{ marginRight: "10px" }} />
+                          Profile
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/profile">
+                          <GrList style={{ marginRight: "10px" }} />
+                          Wishlist
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/profile">
+                          <AiOutlineShoppingCart
+                            style={{ marginRight: "10px", fontSize: "25px" }}
+                          />
+                          {cartItemsCount > 0 && <span>{cartItemsCount}</span>}
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/profile">
+                          <BsBagCheck style={{ marginRight: "10px" }} />
+                          Orders
+                        </a>
+                      </li>
+                    </ul>
+                  </>
+                ) : (
+                  <>
+                    <ul>
+                      <li>
+                        <a href="/signup">Signup</a>
+                      </li>
+                      <li>
+                        <a href="/login">Login</a>
+                      </li>
+                    </ul>
+                  </>
+                )}
+              </div>
+            )}
           </div>
         </nav>
       </Mobile>
-
     </>
-
   );
 }
 
---
+++ client/src/components/SearchInput.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import styles from "../styles/navbar.module.css";
+import axios from "axios";
+
+const SearchInput = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [product, setProduct] = useState([]);
+  const [filterData, setFilterData] = useState([]);
+
+  //   making filter function
+  const filter = (str) => {
+    let filteredData =
+      str === ""
+        ? []
+        : product.filter((item) => {
+            return item.title.toLowerCase().includes(str);
+          });
+
+    setFilterData(filteredData);
+  };
+
+  console.log(filterData, "filterdataa");
+
+  //   handeling serach
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+    let query = event.target.value.toLowerCase();
+    filter(query);
+  };
+
+  //handeling single product
+  const handleProduct = (id) => {
+    console.log(id);
+  };
+
+  //handeling all products
+  const handleAllProduct = () => {
+    console.log(inputValue);
+  };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/products/get")
+      .then((res) => {
+        const productData = res.data.products;
+        setProduct(productData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div className={styles.inputSearch}>
+        <input
+          type="text"
+          name="search"
+          autocomplete="off"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+      </div>
+      {filterData.length > 0 && (
+        <div className={styles.searchResponse}>
+          <div className={styles.scroll_feature}>
+            {filterData?.map((el) => (
+              <div
+                key={el._id}
+                className={styles.searched_product}
+                onClick={() => handleProduct(el._id)}
+              >
+                <div>{el.title}</div>
+                <div>
+                  <img src={el.image} alt="product" />
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className={styles.seeAll} onClick={handleAllProduct}>
+            See All..
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchInput;
