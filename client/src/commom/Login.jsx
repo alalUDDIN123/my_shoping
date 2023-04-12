580d2b669ffd3c4370ba9eb30f997819c67a4851
Alal Uddin <alalu75666@gmail.com>
2023-04-12 20:07:02 +0530

    authentication

--- client/src/commom/Login.jsx
+++ client/src/commom/Login.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react';
+import { FaEnvelope, FaEye, FaEyeSlash } from 'react-icons/fa';
+import {Link} from 'react-router-dom'
+import styles from '../styles/authentication.module.css';
 
-function Login() {
+
+const Login = () => {
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div>
-      Login
+    <div className={styles.form_wrapper}>
+      <div className={styles.form_container}>
+        <div className={styles.title_container}>
+          <h2>Signin Form</h2>
+        </div>
+        <div className={`${styles.row} ${styles.clearfix}`}>
+          <div>
+            <form>
+              
+              <div className={styles.input_field}>
+                <span><FaEnvelope /></span>
+                <input type="email" name="email" placeholder="Email" required />
+              </div>
+              <div className={styles.input_field}>
+                {/* <span><FaLock /></span> */}
+                <input type={showPassword ? 'text' : 'password'} name="password" placeholder="Password" required />
+                <span onClick={handleTogglePassword}>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
+              </div>
+              <input className={styles.button} type="submit" value="Login" />
+              <p>Forget Password ?<Link to="/forgetPassword">Click Here</Link> </p>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- client/src/commom/Signup.jsx
+++ client/src/commom/Signup.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React,{useState} from 'react';
+import { FaUser, FaEnvelope, FaEye, FaEyeSlash } from 'react-icons/fa';
+import { BiUserCircle } from 'react-icons/bi';
+import styles from '../styles/authentication.module.css';
 
-function Signup() {
+
+const Signup = () => {
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div>
-      Signup
-    </div>
-  )
-}
+    <div className={styles.form_wrapper}>
+      <div className={styles.form_container}>
+        <div className={styles.title_container}>
+          <h2>Signup Form</h2>
+        </div>
+        <div className={`${styles.row} ${styles.clearfix}`}>
+          <div>
+            <form>
+              <div className={`${styles.row} ${styles.clearfix}`}>
+                <div className={styles.col_half}>
+                  <div className={styles.input_field}>
+                    <span><FaUser /></span>
+                    <input type="text" name="name" placeholder="First Name" />
+                  </div>
+                </div>
+                <div className={styles.col_half}>
+                  <div className={styles.input_field}>
+                    <span><FaUser /></span>
+                    <input type="text" name="name" placeholder="Last Name" required />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.input_field}>
+                <span><FaEnvelope /></span>
+                <input type="email" name="email" placeholder="Email" required />
+              </div>
+              <div className={styles.input_field}>
+                {/* <span><FaLock /></span> */}
+                <input type={showPassword ? 'text' : 'password'} name="password" placeholder="Password" required />
+                <span onClick={handleTogglePassword}>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
+              </div>
+              <div className={styles.input_field}>
+                <span><BiUserCircle /></span>
+                <input type="url" name="avator" placeholder="Paste your photo link" required />
+              </div>
 
-export default Signup
+
+              <input className={styles.button} type="submit" value="Register" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
