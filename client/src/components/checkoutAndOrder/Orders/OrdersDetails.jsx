8fa0b16e05561921212d78f2a8ec8a032692283d
Ravi Ranjan <ravi047raj@gmail.com>
2023-05-03 20:15:07 +0530

    ordered-deletion (#84)

--- client/src/components/checkoutAndOrder/Orders/OrdersDetails.jsx
+++ client/src/components/checkoutAndOrder/Orders/OrdersDetails.jsx
@@ -4,13 +4,14 @@ import styles from "./orderDetails.module.css";
 import getLoggedUserData from "../../../utils/LoggedUserData";
 import { AiOutlineDownload } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
-import { GetSingleOrderAction, deleteOrderAtion } from "../../../redux/AppReducer/orders/actions";
+import {
+  GetSingleOrderAction,
+  deleteOrderAtion,
+} from "../../../redux/AppReducer/orders/actions";
 import Loader from "../../loader/Loader";
 import { FaTrashAlt } from "react-icons/fa";
 import { toast } from "react-toastify";
 import NoOrderFound from "./NoOrderFound";
-
-
 
 function OrdersDetails() {
   const LoggedUser = getLoggedUserData();
@@ -27,10 +28,8 @@ function OrdersDetails() {
   // find the index of the "orderId" and "productId" strings
   const orderIdIndex = urlParts.indexOf("orderId") + 1;
 
-
   // extract the orderId and productId from the url
   const orderId = urlParts[orderIdIndex];
-
 
   const getUserDetails = async (userToken) => {
     let res = await fetch("http://localhost:8080/api/users/get/user", {
@@ -50,7 +49,6 @@ function OrdersDetails() {
     const payload = {
       token: LoggedUser.token,
       orderId,
-
     };
 
     dispatch(GetSingleOrderAction(payload));
@@ -67,15 +65,16 @@ function OrdersDetails() {
   if (timer) {
     return <Loader />;
   }
-  if (storeData?.order?.message==="Order not found" || storeData?.isError === undefined) {
-    return <NoOrderFound />
+  if (
+    storeData?.order?.message === "Order not found" ||
+    storeData?.isError === undefined
+  ) {
+    return <NoOrderFound />;
   }
 
   const handleDownload = () => {
     alert("Download feature will be added");
   };
-
-
 
   /// handle delete order
 
@@ -83,27 +82,25 @@ function OrdersDetails() {
     const payload = {
       orderId: orderId,
       productId: id,
-      token: LoggedUser.token
-    }
+      token: LoggedUser.token,
+    };
 
     try {
-      let responseFromBackend = await dispatch(deleteOrderAtion(payload))
+      let responseFromBackend = await dispatch(deleteOrderAtion(payload));
       console.log(responseFromBackend, "response from backend");
       if (responseFromBackend === undefined) {
-        throw new Error("Something went wrong")
+        throw new Error("Something went wrong");
       }
 
       if (responseFromBackend && responseFromBackend.hint === "orderRemoved") {
-        toast.success(responseFromBackend.message, { autoClose: 1500 })
+        toast.success(responseFromBackend.message, { autoClose: 1500 });
       } else if (responseFromBackend.hint === "productRe") {
-        toast.success(responseFromBackend.message, { autoClose: 1500 })
+        toast.success(responseFromBackend.message, { autoClose: 1500 });
       }
     } catch (error) {
-      toast.error(error.message, { autoClose: 1500 })
+      toast.error(error.message, { autoClose: 1500 });
     }
-
-
-  }
+  };
   return (
     <>
       <DocumentTitle pageTitle={"| ORDERS | DETAILS"} />
@@ -133,30 +130,36 @@ function OrdersDetails() {
       </div>
 
       <div className={styles.__orderDetails__Order_info}>
-        {storeData?.order?.products.map((el,index) => (
-          <>
-            <div className={styles.__orderDetails__Order_Info_main_container} key={index} >
-              <div >
-                <img src={el.productId.image} alt={el.productId.title} />
-                <p>{el.productId.title}</p>
-              </div>
+        {storeData?.order?.products &&
+          storeData.order.products.map((el, index) => (
+            <>
+              <div
+                className={styles.__orderDetails__Order_Info_main_container}
+                key={index}
+              >
+                <div>
+                  <img src={el.productId.image} alt={el.productId.title} />
+                  <p>{el.productId.title}</p>
+                </div>
 
-              <div>
-                <p>Payment method : {storeData?.order?.paymentMethod}</p>
-                <p>Order Status : {storeData?.order?.orderStatus}</p>
-                <p>Price : ₹ {el.productId.discountPrice}</p>
-              </div>
+                <div>
+                  <p>Payment method : {storeData?.order?.paymentMethod}</p>
+                  <p>Order Status : {storeData?.order?.orderStatus}</p>
+                  <p>Price : ₹ {el.productId.discountPrice}</p>
+                </div>
 
-              <div>
-                <FaTrashAlt onClick={() => handleOrderDelete(el.productId._id)} />
+                <div>
+                  <FaTrashAlt
+                    onClick={() => handleOrderDelete(el.productId._id)}
+                  />
+                </div>
+                <div>
+                  <p>Seller: Mr. Ravi Shop </p>
+                  <p>Rate and review</p>
+                </div>
               </div>
-              <div>
-                <p>Seller: Mr. Ravi Shop </p>
-                <p>Rate and review</p>
-              </div>
-            </div>
-          </>
-        ))}
+            </>
+          ))}
       </div>
 
       {/* <div className={styles.__orderDetails__info_download_invoice_mobile}>
@@ -170,5 +173,3 @@ function OrdersDetails() {
 }
 
 export default OrdersDetails;
-
-
