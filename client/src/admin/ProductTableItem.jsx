ebee63ed06eb6578c15f2023e3fb9654e5ac5fd6
Ravi Ranjan <ravi047raj@gmail.com>
2023-05-07 20:00:44 +0530

    admin-pagination (#85)

--- client/src/admin/ProductTableItem.jsx
+++ client/src/admin/ProductTableItem.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
+import React from "react";
 import { FiEdit } from "react-icons/fi";
 import { FaTrashAlt } from "react-icons/fa";
 
-function ProductTableItem({ ind, image, _id }) {
+function ProductTableItem({ ind, image, _id, startIndex }) {
+  // handle edit
 
-    // handle edit
+  const editProduct = (id) => {
+    alert(`Edit product ${id}`);
+  };
 
-    const editProduct = (id) => {
-        alert(`Edit product ${id}`)
-    }
+  // handle delete product
 
-    // handle delete product
+  const deleteProduct = (id) => {
+    alert(`Delete product ${id}`);
+  };
 
-    const deleteProduct = (id) => {
-        alert(`Delete product ${id}`)
-    }
-
-
-    return (
-        <>
-            <tr key={ind}>
-                <td>{ind + 1}</td>
-                <td>
-                    <img src={image} alt={_id} />
-                </td>
-                <td>{_id}</td>
-                <td onClick={() => editProduct(_id)} ><FiEdit /></td>
-                <td onClick={() => deleteProduct(_id)} ><FaTrashAlt /></td>
-            </tr>
-        </>
-    )
+  return (
+    <>
+      <tr key={startIndex + ind}>
+        <td>{startIndex + ind + 1}</td>
+        <td>
+          <img src={image} alt={_id} />
+        </td>
+        <td>{_id}</td>
+        <td onClick={() => editProduct(_id)}>
+          <FiEdit />
+        </td>
+        <td onClick={() => deleteProduct(_id)}>
+          <FaTrashAlt />
+        </td>
+      </tr>
+    </>
+  );
 }
 
-export default ProductTableItem
+export default ProductTableItem;
--- client/src/admin/Products.jsx
+++ client/src/admin/Products.jsx
@@ -1,61 +1,68 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import TopSection from './TopSection';
-import { getProductsData } from '../redux/AppReducer/products/actions';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import TopSection from "./TopSection";
+import { getProductsData } from "../redux/AppReducer/products/actions";
 import { BsBoxFill } from "react-icons/bs";
 import { AiOutlineFundView } from "react-icons/ai";
 import { GrLinkPrevious, GrLinkNext } from "react-icons/gr";
-import Loader from '../components/loader/Loader';
-import styles from "./styles/products.module.css"
-import ProductTableItem from './ProductTableItem';
+import Loader from "../components/loader/Loader";
+import styles from "./styles/products.module.css";
+import ProductTableItem from "./ProductTableItem";
 
-// I want complete data 
+// I want complete data
 
 const Products = () => {
-  const [page, setPage] = useState(1)
-  const [limit, setLimit] = useState(5)
-  const dispatch = useDispatch()
-  const { products, isLoading , totalProductspage} = useSelector((store) => store.getProductReducer);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(5);
+  const dispatch = useDispatch();
+  const { products, isLoading, totalProductspage } = useSelector(
+    (store) => store.getProductReducer
+  );
 
-  let productsLegth = Number(localStorage.getItem("productsLegth")) || 0
-  let outOfStockLentgh = Number(localStorage.getItem("outOfStock")) || 0
+  // pagination logic
+  const totalpage = totalProductspage;
+  const startIndex = (page - 1) * limit;
+
+  let productsLegth = Number(localStorage.getItem("productsLegth")) || 0;
+  let outOfStockLentgh = Number(localStorage.getItem("outOfStock")) || 0;
   // console.log(outOfStock);
   const data = [
     {
       title: "Total Products",
       count: productsLegth,
-      icon: <BsBoxFill />
+      icon: <BsBoxFill />,
     },
     {
       title: "In Stock",
       count: productsLegth - outOfStockLentgh,
-      icon: <AiOutlineFundView />
+      icon: <AiOutlineFundView />,
     },
     {
       title: "Out Of Stock",
       count: outOfStockLentgh,
-      icon: <AiOutlineFundView />
-    }
-  ]
+      icon: <AiOutlineFundView />,
+    },
+  ];
 
-// total length=100
-// totalpage=20
-// limit =5
+  // total length=100
+  // totalpage=20
+  // limit =5
 
   useEffect(() => {
     const payload = {
-      page, limit
-    }
-    dispatch(getProductsData(payload))
-  }, [dispatch, limit, page, setPage, setLimit])
-
-console.log(" totalProductspage:", totalProductspage);
+      page,
+      limit,
+    };
+    dispatch(getProductsData(payload));
+  }, [dispatch, limit, page, setPage, setLimit]);
 
   return (
     <>
       <TopSection data={data} />
-      {isLoading ? <Loader /> : (
-        <table className={styles.__admin__product__table} >
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <table className={styles.__admin__product__table}>
           <thead>
             <tr>
               <th>S/N</th>
@@ -67,29 +74,34 @@ console.log(" totalProductspage:", totalProductspage);
           </thead>
 
           <tbody className={styles.__admin__product__body}>
-            {products.length > 0 && products?.map((el, ind) => (
-              <ProductTableItem key={ind} {...el} ind={ind} />
-            ))}
+            {products.length > 0 &&
+              products?.map((el, ind) => (
+                <ProductTableItem
+                  key={ind}
+                  {...el}
+                  ind={ind}
+                  startIndex={startIndex}
+                />
+              ))}
           </tbody>
         </table>
       )}
 
       {/* pagination */}
-      <div className={styles.__admin__product__pagi} >
+      <div className={styles.__admin__product__pagi}>
         <button disabled={page === 1} onClick={() => setPage(page - 1)}>
           <GrLinkPrevious />
         </button>
         <p>{page}</p>
         <button
-          
+          disabled={page === totalpage ? true : false}
           onClick={() => setPage(page + 1)}
         >
           <GrLinkNext />
         </button>
       </div>
-
     </>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
